dcbb8003f2a55317f13bb1ccccda9b12e1993641
윤태형 <howard9166@naver.com>
2026-05-07 13:06:15 +0900

    [level 3] Title: 정수 삼각형, Time: 6.21 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int N = triangle.length;
+        int [][] dp =  new int[N][N];
+        
+        dp[0][0]=triangle[0][0];
+        
+        for(int i = 1 ; i< N ; i ++) {
+        	for(int j = 0;j<=i;j++) {
+        		if(j==0) dp[i][j]=dp[i-1][j]+triangle[i][j];
+        		else if (j==i) dp[i][j]=dp[i-1][j-1]+triangle[i][j];
+        		else dp[i][j]=Math.max(dp[i-1][j-1],dp[i-1][j])+triangle[i][j];
+        	}
+        }
+        int maxsum = -1;
+        
+//        for(int i = 0 ; i< N ; i ++) {
+//        	for(int j = 0;j<=i;j++) {
+//        		System.out.printf("%d ",dp[i][j]);
+//        	}
+//        	System.out.println("");
+//        }
+        
+        for(int i = 0 ; i< N ; i ++) {
+        	if(maxsum<dp[N-1][i]) maxsum=dp[N-1][i];
+        }
+        return maxsum;
+    }
+}
