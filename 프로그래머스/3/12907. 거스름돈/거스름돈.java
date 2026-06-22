7ca67daf42f3c5182dc0c5b5250f27806bd8abc0
윤태형 <howard9166@naver.com>
2026-06-22 15:32:31 +0900

    [level 3] Title: 거스름돈, Time: 11.68 ms, Memory: 57.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int answer = 0;
+        
+		int [] dp = new int [ n + 1 ]; 
+		dp[0] = 1;
+		
+		for (int coin : money) {
+            for (int j = coin; j <= n; j++) {
+                dp[j] = (dp[j] + dp[j - coin]) % 1000000007;
+            }
+        }
+        return dp[n];
+    }
+}
